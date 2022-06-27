c0ca08c50bbcbb0c4dd91292fd56f6d735e23eb2
Jake Schwartz <jakehschwartz@gmail.com>
2022-06-27 13:00:17 -0400

    Powerline font install setup

--- setup.sh
+++ setup.sh
@@ -8,6 +8,15 @@ xcode-select --install
 # Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+# clone
+git clone https://github.com/powerline/fonts.git --depth=1
+# install
+cd fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
+
 # Install SDKMAN
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
